cdf4c0b1a153716f41831d1028b68e948cb0ef66
Alf Gaida <agaida@siduction.org>
2016-11-04 23:16:38 +0100

    Adding upstream version 0.11.0-86-g6d2de75.
    Signed-off-by: Alf Gaida <agaida@siduction.org>

---
+++ lxqt-panel/plugin-mainmenu/mainmenu_fr.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>LXQtMainMenu</name>
+    <message>
+        <location filename="../../../lxqtmainmenu.cpp" line="95"/>
+        <source>Search...</source>
+        <translation>Recherche...</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenu.cpp" line="101"/>
+        <source>Show/hide main menu</source>
+        <translation>Afficher/Cacher le menu principal</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtMainMenuConfiguration</name>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="20"/>
+        <source>General</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="14"/>
+        <source>Main Menu settings</source>
+        <translation>Paramétrage du menu principal</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="35"/>
+        <source>Icon:</source>
+        <translation>Icône :</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="59"/>
+        <source>Button text:</source>
+        <translation>Texte du bouton :</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="76"/>
+        <source>Custom font size:</source>
+        <translation>Taille personnalisée des fontes :</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="86"/>
+        <source> pt</source>
+        <translation> pt</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="105"/>
+        <source>Menu file</source>
+        <translation>Fichier de menu</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="111"/>
+        <source>Menu file:</source>
+        <translation>Fichier de menu :</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="169"/>
+        <source>Search</source>
+        <translation>Recherche</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="175"/>
+        <source>max. item width:</source>
+        <translation>Largeur maxi des items :</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="182"/>
+        <source>Show matching entries:</source>
+        <translation>Afficher les entrées concordantes :</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="196"/>
+        <source>Maximum visible items:</source>
+        <translation>Maximum d&apos;items visibles :</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="203"/>
+        <source>Filter menu entries</source>
+        <translation>Filtrer les entrées de menu</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="210"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="223"/>
+        <source>Hide menu entries while searching</source>
+        <translation>Cacher les entrées pendant la recherche</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="127"/>
+        <source>Keyboard Shortcut</source>
+        <translation>Raccourcis clavier</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.ui" line="146"/>
+        <source>Click the button to record shortcut:</source>
+        <translation>Cliquez sur le bouton pour enregistrer le raccourci :</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="71"/>
+        <source>Reset</source>
+        <translation>Réinitialiser</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="156"/>
+        <source>Choose icon file</source>
+        <translation>Choisir un fichier d&apos;icône</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="158"/>
+        <source>Images (*.svg *.png)</source>
+        <translation>Images (*.svg *.png)</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="170"/>
+        <source>Choose menu file</source>
+        <translation>Choisissez un fichier de menu</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqtmainmenuconfiguration.cpp" line="172"/>
+        <source>Menu files (*.menu)</source>
+        <translation>Fichiers de menu (*.menu)</translation>
+    </message>
+</context>
+</TS>
---
+++ lxqt-panel/plugin-taskbar/taskbar_fr.ts
@@ -0,0 +1,209 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>LXQtTaskButton</name>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="477"/>
+        <source>Application</source>
+        <translation>Application</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="511"/>
+        <source>To &amp;Desktop</source>
+        <translation>Vers &amp;le bureau</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="513"/>
+        <source>&amp;All Desktops</source>
+        <translation>&amp;Tous les bureaux</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="521"/>
+        <source>Desktop &amp;%1</source>
+        <translation>Bureau &amp;%1</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="528"/>
+        <source>&amp;To Current Desktop</source>
+        <translation>&amp;Vers le bureau courant</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="537"/>
+        <source>Ma&amp;ximize</source>
+        <translation>Ma&amp;ximiser</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="544"/>
+        <source>Maximize vertically</source>
+        <translation>Maximiser verticalement</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="549"/>
+        <source>Maximize horizontally</source>
+        <translation>Maximiser horizontalement</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="555"/>
+        <source>&amp;Restore</source>
+        <translation>&amp;Restaurer</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="559"/>
+        <source>Mi&amp;nimize</source>
+        <translation>Mi&amp;nimiser</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="565"/>
+        <source>Roll down</source>
+        <translation>Enrouler vers le bas</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="571"/>
+        <source>Roll up</source>
+        <translation>Enrouler vers le haut</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="579"/>
+        <source>&amp;Layer</source>
+        <translation>&amp;Disposition</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="581"/>
+        <source>Always on &amp;top</source>
+        <translation>Toujours au &amp;dessus</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="587"/>
+        <source>&amp;Normal</source>
+        <translation>&amp;Normal</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="593"/>
+        <source>Always on &amp;bottom</source>
+        <translation>Toujours en &amp;bas</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbutton.cpp" line="601"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Fermer</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtTaskGroup</name>
+    <message>
+        <location filename="../../../lxqttaskgroup.cpp" line="83"/>
+        <source>Group</source>
+        <translation>Groupe</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskgroup.cpp" line="85"/>
+        <source>Close group</source>
+        <translation>Fermer le groupe</translation>
+    </message>
+</context>
+<context>
+    <name>LXQtTaskbarConfiguration</name>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="14"/>
+        <source>Task Manager Settings</source>
+        <translation>Paramétrage du gestionnaire de tâches</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="20"/>
+        <source>General</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="34"/>
+        <source>Show only windows from desktop</source>
+        <translation>Afficher uniquement les fenêtres du bureau</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="46"/>
+        <source>Show only windows from &amp;panel&apos;s screen</source>
+        <translation>Afficher uniquement les fenêtres de l&apos;écran du &amp;panneau</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="53"/>
+        <source>Show only minimized windows</source>
+        <translation>Afficher uniquement les fenêtres minimisées</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="60"/>
+        <source>Raise minimized windows on current desktop</source>
+        <translation>Relever les fenêtres  réduites du bureau actuel</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="77"/>
+        <source>Window &amp;grouping</source>
+        <translation>&amp;Groupement de fenêtres</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="86"/>
+        <source>Show popup on mouse hover </source>
+        <translation>Afficher un pop-up lorsque la souris survole</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="96"/>
+        <source>Appearance</source>
+        <translation>Apparence</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="115"/>
+        <source>Maximum button width</source>
+        <translation>Largeur maximale des boutons</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="128"/>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="154"/>
+        <source> px</source>
+        <translation> px</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="141"/>
+        <source>Maximum button height</source>
+        <translation>Hauteur maximale des boutons</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="174"/>
+        <source>Use icons by WindowClass, if available</source>
+        <translation>Utilisez les icônes par WindowClass, si disponible</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="167"/>
+        <source>Auto&amp;rotate buttons when the panel is vertical</source>
+        <translation>&amp;Rotation automatique des boutons lorsque le tableau est vertical</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="105"/>
+        <source>Button style</source>
+        <translation>Style de boutons</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.ui" line="67"/>
+        <source>Close on middle-click</source>
+        <translation>Fermer d&apos;un clic du milieu</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.cpp" line="45"/>
+        <source>Icon and text</source>
+        <translation>Icône et texte</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.cpp" line="46"/>
+        <source>Only icon</source>
+        <translation>Icône seule</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.cpp" line="47"/>
+        <source>Only text</source>
+        <translation>Texte seul</translation>
+    </message>
+    <message>
+        <location filename="../../../lxqttaskbarconfiguration.cpp" line="49"/>
+        <source>Current</source>
+        <translation>Courant</translation>
+    </message>
+</context>
+</TS>
