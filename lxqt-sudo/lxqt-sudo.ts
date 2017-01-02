e448775070b982e6582d541faad9405f90b37338
Alf Gaida <agaida@siduction.org>
2017-01-02 11:09:44 +0100

    Cherry-picking upstream version 0.11.2.
    * Bumped years in d/copyright
    * Bumped build dependency lxqt-build-tools (>= 0.3.1)

--- lxqt-sudo/lxqt-sudo.ts
+++ lxqt-sudo/lxqt-sudo.ts
@@ -1,34 +1,33 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="uk">
+<TS version="2.1">
 <context>
     <name>PasswordDialog</name>
     <message>
         <location filename="../../passworddialog.ui" line="20"/>
         <source>LXQt sudo</source>
-        <translation>sudo для LXQT</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../passworddialog.ui" line="77"/>
         <source>Command:</source>
-        <translation>Команда:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../passworddialog.ui" line="84"/>
         <source>Password:</source>
-        <translation>Пароль:</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../passworddialog.cpp" line="43"/>
         <source>&lt;b&gt;%1&lt;/b&gt; needs administrative privileges.
 Please enter your password.</source>
-        <translation>&lt;b&gt;%1&lt;/b&gt; вимагає прав адміністратора.
-Будь ласка, введіть Ваш пароль.</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../passworddialog.cpp" line="54"/>
         <source>Attempt #%1</source>
-        <translation>Спроба #%1 </translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -49,27 +48,13 @@ Arguments:
   arguments        Optional arguments for command.
 
 </source>
-        <translation>Використання: %1 опція [команда [аргументи...]]
-
-Накладка GUI для %2/%3
-
-Аргументи:
-  опція:
-    -h|--help      Надрукувати цю допомогу.
-    -v|--version   Надрукувати інформацію про версію.
-    -s|--su        Використати %3(1) як бекенд.
-    -d|--sudo      Використати %2(8) як бекенд.
-  команда          Команда для запуску.
-  аргументи        Опціональні аргументи для команди.
-
-</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../sudo.cpp" line="80"/>
         <source>%1 version %2
 </source>
-        <translation>%1 версія %2
-</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -77,46 +62,44 @@ Arguments:
     <message>
         <location filename="../../sudo.cpp" line="135"/>
         <source>%1: no command to run provided!</source>
-        <translation>%1: не вказано команди для запуску!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../sudo.cpp" line="142"/>
         <source>%1: no backend chosen!</source>
-        <translation>%1: не вибрано бекенд!</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../sudo.cpp" line="147"/>
         <source>%1: warning - got multiple arguments for %2 backend, squashing into one: %3</source>
-        <translation type="unfinished">%1: увага - отримано багато аргументів для %2 бекенду, що зливаються в один: %3</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../sudo.cpp" line="162"/>
         <source>Failed to fork: %1</source>
-        <translation>Неможливо розгалузити: %1 </translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../sudo.cpp" line="205"/>
         <source>%1: Failed to exec &apos;%2&apos;: %3
 </source>
-        <translation>%1: Помилка запуску &apos;%2&apos;: %3
-</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../sudo.cpp" line="234"/>
         <source>Failed to set non-block: %1</source>
-        <translation>Не в змозі встановити не-блок: %1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../sudo.cpp" line="242"/>
         <source>Failed to fdopen: %1</source>
-        <translation>Не в стані відкрити: %1</translation>
+        <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../sudo.cpp" line="275"/>
         <source>Child &apos;%1&apos; process failed!
 %2</source>
-        <translation>Підпроцес &apos;%1&apos; помилково завершився!
-%2</translation>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
